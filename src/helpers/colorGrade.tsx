f047166bb9fd825f086aea6c51d953c2db3c7534
LaneLR <laneleerichardson@gmail.com>
2026-01-16 10:43:33 -0600

    change colors of grades

--- src/helpers/colorGrade.tsx
+++ src/helpers/colorGrade.tsx
@@ -1,8 +1,8 @@
 export default function getGradeColor (grade: string) {
     const g = grade?.toUpperCase();
-    if (g === "A") return "#a855f7";
-    if (g === "B") return "#2431e9";
-    if (g === "C") return "#eab308";
-    if (g === "D") return "#f97316";
-    return "#ef4444";
+    if (g === "A") return "#ff6000";
+    if (g === "B") return "#ff6000";
+    if (g === "C") return "#ff6000";
+    if (g === "D") return "#bb0000";
+    return "#bb0000";
   };
