3663304949f897f3ebbfeb52439addbbb7680e6a
LaneLR <laneleerichardson@gmail.com>
2026-01-19 23:25:58 -0600

    prep for ios build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isAppBuild = process.env.IS_APP_BUILD === 'true';
+
 const nextConfig = {
   // Uncomment the line below when you are ready to build for Capacitor/Xcode
-  output: 'export', 
+  output: isAppBuild ? 'export' : undefined, 
   
-  trailingSlash: false,
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
