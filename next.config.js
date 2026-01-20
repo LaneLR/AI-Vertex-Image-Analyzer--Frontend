eb87d37ce4254543f867fb64f6ce00246e9f8539
LaneLR <laneleerichardson@gmail.com>
2026-01-19 23:17:00 -0600

    prep for ios build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Uncomment the line below when you are ready to build for Capacitor/Xcode
+  // output: 'export', 
+  
   trailingSlash: false,
   eslint: {
     ignoreDuringBuilds: true,
@@ -11,6 +14,21 @@ const nextConfig = {
     remotePatterns: [],
     unoptimized: true,
   },
+  
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
+
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = config.externals || [];
@@ -18,7 +36,6 @@ const nextConfig = {
     }
     return config;
   },
-  // output: 'export',
 };
 
 module.exports = nextConfig;
