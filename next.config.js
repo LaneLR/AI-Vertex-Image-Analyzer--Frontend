e284f719ef26e8091c6b7ccaaf343ca16d284882
LaneLR <laneleerichardson@gmail.com>
2026-02-04 19:50:58 -0600

    fix next.config for ios build

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const isAppBuild = process.env.IS_APP_BUILD === "true";
 
 const nextConfig = {
   // Uncomment the line below when you are ready to build for Capacitor/Xcode
-  // output: "export", // Force this manually for a second to test locally
-  // distDir: "out", // Ensure this matches your capacitor.config.ts
-  trailingSlash: false,
+  output: "export", // Force this manually for a second to test locally
+  distDir: "out", // Ensure this matches your capacitor.config.ts
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
